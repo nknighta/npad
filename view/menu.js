fdac27dbb8c8c6a0c2bc85a3ca590b746e2ed4f7
nknighta <n.knight.pc0627@gmail.com>
2024-04-24 17:17:50 +0900

    ✨ : feat - ssettings window

---
+++ view/menu.js
@@ -0,0 +1,8 @@
+const files_ui = document.getElementById('menu-ui-files');
+
+let isMenuOpend = false;
+const menuElement = document.createElement("div");
+
+const menuElementTextTest = document.createTextNode("OMG");
+
+menuElement.appendChild(menuElementTextTest);
